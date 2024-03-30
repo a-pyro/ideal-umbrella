41808e85d664618af307d4cf1c1748c10a83a66c
Ardi Germenji <ardi.germenji@interlogica.it>
2024-03-30 15:04:10 +0000

    Refactor product detail view component

--- app/products/components/product-detail-view.tsx
+++ app/products/components/product-detail-view.tsx
@@ -1,5 +1,5 @@
 /* eslint-disable @next/next/no-img-element -- no need */
-import { Box, Stack } from '@mui/material'
+import { Box, Divider, Stack, Typography } from '@mui/material'
 
 import { GridContainer, GridItem } from '@/components'
 import type { Product } from '@/services/api/products'
@@ -14,40 +14,62 @@ export const ProductDetailView = ({ product }: Props) => {
   const lastImage = product.images[product.images.length - 1]
 
   return (
-    <GridContainer spacing={2}>
-      <GridItem md={4} sm={12} xs={12}>
-        <img
-          alt={product.title}
-          src={firstImage}
-          style={{ height: '100%', objectFit: 'cover', width: '100%' }}
-        />
-      </GridItem>
-
-      {middleImages.length > 0 && (
-        <GridItem md={4} sm={12} xs={12}>
-          <Stack>
-            {middleImages.map((img, idx) => (
-              <Box key={img}>
-                <img
-                  alt={`${product.title} ${idx + 1}`}
-                  src={img}
-                  style={{ height: '100%', objectFit: 'cover', width: '100%' }}
-                />
-              </Box>
-            ))}
-          </Stack>
-        </GridItem>
-      )}
-
-      {product.images.length > 1 && (
+    <Stack spacing={2}>
+      <GridContainer spacing={2}>
         <GridItem md={4} sm={12} xs={12}>
           <img
-            alt={`${product.title} pic`}
-            src={lastImage}
+            alt={product.title}
+            src={firstImage}
             style={{ height: '100%', objectFit: 'cover', width: '100%' }}
           />
         </GridItem>
-      )}
-    </GridContainer>
+
+        {middleImages.length > 0 && (
+          <GridItem md={4} sm={12} xs={12}>
+            <Stack>
+              {middleImages.map((img, idx) => (
+                <Box key={img}>
+                  <img
+                    alt={`${product.title} ${idx + 1}`}
+                    src={img}
+                    style={{
+                      height: '100%',
+                      objectFit: 'cover',
+                      width: '100%',
+                    }}
+                  />
+                </Box>
+              ))}
+            </Stack>
+          </GridItem>
+        )}
+
+        {product.images.length > 1 && (
+          <GridItem md={4} sm={12} xs={12}>
+            <img
+              alt={`${product.title} pic`}
+              src={lastImage}
+              style={{ height: '100%', objectFit: 'cover', width: '100%' }}
+            />
+          </GridItem>
+        )}
+      </GridContainer>
+
+      <Stack
+        direction="row"
+        divider={<Divider flexItem orientation="vertical" />}
+      >
+        <Stack flex={2} mr="auto" px={2}>
+          <Typography fontWeight={600} variant="h5">
+            {product.title}
+          </Typography>
+          <Typography>{product.description}</Typography>
+        </Stack>
+        <Stack flex={1} px={2}>
+          <Typography variant="h6">{product.priceLabel}</Typography>
+          <Typography variant="h6">{product.rating}</Typography>
+        </Stack>
+      </Stack>
+    </Stack>
   )
 }
