e7cd7b6f9afcc5a54914a844bb1b29499584572a
a-pyro <ardi.germenji@gmail.com>
2024-04-01 20:51:38 +0100

    Refactor cart drawer component

--- components/cart-drawer.tsx
+++ components/cart-drawer.tsx
@@ -1,6 +1,8 @@
 'use client'
+import AddCircleOutlineIcon from '@mui/icons-material/AddCircleOutline'
 import DeleteForeverIcon from '@mui/icons-material/DeleteForever'
 import HighlightOffIcon from '@mui/icons-material/HighlightOff'
+import RemoveCircleOutlineIcon from '@mui/icons-material/RemoveCircleOutline'
 import ShoppingCartOutlinedIcon from '@mui/icons-material/ShoppingCartOutlined'
 import type { ButtonProps, IconButtonProps } from '@mui/material'
 import {
@@ -14,14 +16,14 @@ import {
 import Drawer from '@mui/material/Drawer'
 import List from '@mui/material/List'
 import ListItem from '@mui/material/ListItem'
-import ListItemButton from '@mui/material/ListItemButton'
-import ListItemIcon from '@mui/material/ListItemIcon'
-import ListItemText from '@mui/material/ListItemText'
-import { useState } from 'react'
+import { useMemo, useState } from 'react'
 
-import { clearCart } from '@/app/api/cart/actions'
+import { addToCart, clearCart, removeFromCart } from '@/app/api/cart/actions'
+import type { Product } from '@/app/api/products/fetch'
 
 import { useCart } from './context'
+
+type CartProduct = Product & { quantity: number }
 
 export const CartDrawer = () => {
   const [isOpen, setIsOpen] = useState(false)
@@ -39,26 +41,34 @@ export const CartDrawer = () => {
       setIsOpen(open)
     }
 
+  const cartProducts = useMemo(
+    () =>
+      cart.products.reduce<CartProduct[]>((acc, product) => {
+        const existingProduct = acc.find((p) => p.id === product.id)
+        if (existingProduct) {
+          return acc.map((p) =>
+            p.id === product.id ? { ...p, quantity: p.quantity + 1 } : p,
+          )
+        }
+        return [...acc, { ...product, quantity: 1 }]
+      }, []),
+    [cart.products],
+  )
+
   return (
     <aside>
       <ShoppingCartButton
         onClick={toggleDrawer(true)}
         totalItems={cart.products.length}
       />
-      <Drawer
-        anchor="right"
-        onClose={toggleDrawer(false)}
-        open={isOpen}
-        variant="persistent"
-      >
+      <Drawer anchor="right" onClose={toggleDrawer(false)} open={isOpen}>
         <Stack
           flexGrow={1}
-          onClick={toggleDrawer(false)}
           onKeyDown={toggleDrawer(false)}
           px={3}
           py={2}
           role="presentation"
-          width={['100vw', '60vw', '30vw']}
+          width={['100vw', '60vw', '40vw']}
         >
           <List>
             <Stack direction="row">
@@ -67,14 +77,24 @@ export const CartDrawer = () => {
                 <HighlightOffIcon />
               </IconButton>
             </Stack>
-            {cart.products.map(({ id, thumbnail, title }) => (
-              <ListItem disablePadding key={id}>
-                <ListItemButton sx={{ pl: 0 }}>
-                  <ListItemIcon>
+            {cartProducts.map(({ id, quantity, thumbnail, title }) => (
+              <ListItem disablePadding key={id} sx={{ mb: 1, pl: 0 }}>
+                <Stack
+                  alignItems="center"
+                  direction="row"
+                  justifyContent="space-between"
+                  width="100%"
+                >
+                  <Stack alignItems="center" direction="row" spacing={2}>
                     <Avatar alt={title} src={thumbnail} />
-                  </ListItemIcon>
-                  <ListItemText primary={title} />
-                </ListItemButton>
+                    <Typography> {title} </Typography>
+                  </Stack>
+                  <QuantityHandler
+                    onAdd={async () => setCart(await addToCart(id))}
+                    onRemove={async () => setCart(await removeFromCart(id))}
+                    quantity={quantity}
+                  />
+                </Stack>
               </ListItem>
             ))}
           </List>
@@ -109,5 +129,29 @@ const ClearCartButton = (props: ButtonProps) => {
     <Button startIcon={<DeleteForeverIcon />} {...props}>
       Svuota carrello
     </Button>
+  )
+}
+
+const QuantityHandler = ({
+  onAdd,
+  onRemove,
+  quantity,
+  sx,
+}: {
+  onAdd: () => void
+  onRemove: () => void
+  quantity: number
+  sx?: ButtonProps['sx']
+}) => {
+  return (
+    <Stack alignItems="center" direction="row" spacing={1} sx={sx}>
+      <IconButton onClick={onRemove}>
+        <RemoveCircleOutlineIcon />
+      </IconButton>
+      <Typography>{quantity}</Typography>
+      <IconButton onClick={onAdd}>
+        <AddCircleOutlineIcon />
+      </IconButton>
+    </Stack>
   )
 }
