d992ffbd7a1f93d3b95ebd5146a3341311fe0abd
a-pyro <ardi.germenji@gmail.com>
2024-04-01 03:22:10 +0100

    Add addToCart functionality to ProductList component

--- app/products/components/product-list.tsx
+++ app/products/components/product-list.tsx
@@ -1,18 +1,22 @@
 'use client'
+import { addToCart } from '@/app/api/products/actions'
 import type { Product } from '@/app/api/products/fetch'
+import { useCart } from '@/components/context'
 import { GridContainer } from '@/components/layout/grid'
 
 import { ProductCard } from './product-card'
 
 export const ProductList = ({ products }: { products: Product[] }) => {
+  const [, setCart] = useCart()
+  const handleAddToCart = (productId: number) => async () =>
+    setCart(await addToCart(productId))
+
   return (
     <GridContainer mx={-1}>
       {products.map((product) => (
         <ProductCard
           key={product.id}
-          onAddToCart={() => {
-            // Add to cart logic
-          }}
+          onAddToCart={handleAddToCart(product.id)}
           product={product}
         />
       ))}
